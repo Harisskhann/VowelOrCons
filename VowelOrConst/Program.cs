54e6d50d1673be487355823ddb8bf4177184b124
M.Haris Khan <hkhan@cedrus.com>
2023-07-20 12:16:05 +0500

    Feat;Check Vowel sand Consonents

---
+++ VowelOrConst/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrConst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str;
+            int i, length, vowel, cons;
+
+            Console.Write("\n\nCount total number of vowel or consonant :\n");
+
+            str = Console.ReadLine();
+
+            vowel = 0;
+            cons = 0;
+            length = str.Length;
+
+            for (i = 0; i < length; i++)
+            {
+
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vowel++;
+                }
+                else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    cons++;
+                }
+            }
+            Console.Write("The total number of vowel in the string is : " + vowel + '\n');
+            Console.Write("The total number of consonant in the string is : " + cons);
+            Console.ReadLine();
+        }
+    }
+
+}
+    
+
